077d3d7095f94be944179d091425982f55a323ae
wdwdwq <qkenr7278@gmail.com>
2024-06-17 15:31:20 +0900

    1부터 n까지 소수의 합 출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,14 @@ package org.example;
 
 import static org.example.Main2.Math2.isPrimeNumber;
 
-//문제 : 1부터 n까지 소수의 객수를 출력하는 함수를 만들어주세요
+//문제 : 1부터 n까지 소수의 합 출력하는 함수를 만들어주세요
 class Main {
     public static void main(String[] args) {
-        int rs = Math.one_to_n_prime_numbers_count(10);
+        int rs = Math.one_to_n_prime_numbers_sum(10);
         System.out.println("rs : " + rs);
         // 출력 : rs : 4
 
-        rs = Math.one_to_n_prime_numbers_count(13);
+        rs = Math.one_to_n_prime_numbers_sum(13);
         System.out.println("rs : " + rs);
         // 출력 : rs : 6
     }
@@ -17,13 +17,13 @@ class Main {
 
 class Math {
 
-     static int one_to_n_prime_numbers_count(int n) {
-        int count = 0; // 값 초기화
+     static int one_to_n_prime_numbers_sum(int n) {
+        int sum = 0; // 값 초기화
         for (int i = 2; i <= n; i++) {
             if(isPrimeNumber(i)){
-                count++;
+                sum += i;
             }
         }
-          return count;
+          return sum;
     }
 }
