29882627c955312a34c2903c67363a477b3be786
wdwdwq <qkenr7278@gmail.com>
2024-06-17 11:26:17 +0900

    문제 - 함수를 실행하면 값을 돌려주도록 만들어주세요.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,35 +2,40 @@ package org.example; // org.example 패키지에 속해 있음
 
 public class Main {
     public static void main(String[] args) {
-        계산기.더한결과를_화면에_출력(10, 20);
+        int 결과;
+
+        결과 = 계산기.합(10, 20);
+        System.out.println("결과 : " + 결과);
         // 출력 => 결과 : 30
 
-        계산기.더한결과를_화면에_출력(50, 20);
-        // 출력 => 결과 : 70
+        결과 = 계산기.합(30, 20);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 50
 
-        계산기.뺀결과를_화면에_출력(50, 20);
-        // 출력 => 결과 : 30
+        결과 = 계산기.합(30, 70);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 100
 
-        계산기.뺀결과를_화면에_출력(5, 2);
-        // 출력 => 결과 : 3
+        결과 = 계산기.차(30, 70);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : -40
 
-        계산기.곱한결과를_화면에_출력(5, 2);
-        // 출력 => 결과 : 10
-    }
-}
-class  계산기 {
-
-    public static void 더한결과를_화면에_출력(int i, int j) {
-        System.out.println(i + j);
-    }
-
-    public static void 뺀결과를_화면에_출력(int i, int j) {
-        System.out.println(i - j);
-    }
-
-    public static void 곱한결과를_화면에_출력(int i, int j) {
-        System.out.println(i * j);
+        결과 = 계산기.곱(3, 7);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 21
     }
 }
 
+class 계산기 {
+    public static int 합(int a, int b) {
+        return a + b;
+    }
 
+    public static int 차(int a, int b) {
+        return a - b;
+    }
+
+    public static int 곱(int a, int b) {
+        return a * b;
+    }
+}
