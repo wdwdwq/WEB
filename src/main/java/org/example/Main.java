fe6684406479948621dc173e890caf3eeee1d8e9
wdwdwq <qkenr7278@gmail.com>
2024-06-17 14:08:21 +0900

    문제 : 1부터 n까지 소수의 객수를 출력하는 함수를 만들어주세요

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,33 @@
-package org.example; // org.example 패키지에 속해 있음
-
-public// 문제 : 사람을 말하게 해주세요.
-
+package org.example;
+//문제 : 1부터 n까지 소수의 객수를 출력하는 함수를 만들어주세요
 class Main {
     public static void main(String[] args) {
-        사람 a사람 = new 사람();
-        a사람.말하다();
+        int rs = Math.one_to_n_prime_numbers_count(10);
+        System.out.println("rs : " + rs);
+        // 출력 : rs : 4
+
+        rs = Math.one_to_n_prime_numbers_count(13);
+        System.out.println("rs : " + rs);
+        // 출력 : rs : 6
     }
 }
 
-class 사람 {
-    // 사람은 `말하다` 라는 기능을 가지고 있습니다.
-    void 말하다() {
-        System.out.println("사람이 말합니다.");
+class Math {
+
+    public static int one_to_n_prime_numbers_count(int n) {
+        int count = 0;
+        for (int num = 2; num <= n; num++) {
+            boolean isPrime = true;
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                count++;
+            }
+        }
+        return count;
     }
 }
-
--- src/main/java/org/example/Main2.java
+++ src/main/java/org/example/Main2.java
@@ -1,21 +1,45 @@
-package org.example; // org.example 패키지에 속해 있음
+package org.example;
 
-public // 문제 : 아래 코드가 실행되도록 해주세요.
-
-class Main2 {
+public class Main2 {
+    //문제 : 1부터 n까지 소수의 객수를 출력하는 함수를 만들어주세요
     public static void main(String[] args) {
-        거북이 a거북이 = new 거북이();
+        boolean rs = Math2.isPrimeNumber(1);
+        System.out.println("1은(는) 소수인가? : " + rs);
+        //false
+        rs = Math2.isPrimeNumber(2);
+        System.out.println("2은(는) 소수인가? : " + rs);
+        // ture
+        rs = Math2.isPrimeNumber(4);
+        System.out.println("4은(는) 소수인가? : " + rs);
+        //false
+        rs = Math2.isPrimeNumber(7);
+        System.out.println("7은(는) 소수인가? : " + rs);
+        //true
+    }
 
-        a거북이.걷다();
-        a거북이.수영하다();
-    }
-}
-class 거북이 {
-    void 걷다() {
-        System.out.println("걷다");
+    class Math2 {
+         static boolean isPrimeNumber(int n) {
+            if (n <= 1) {
+                return false;
+            }
+            if (n == 2) {
+                return true;
+            }
+            if (n % 2 == 0) {
+                return false;
+            }
+            for (int i = 3; i * i <= n; i += 2) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
-    void 수영하다() {
-        System.out.println("수영하다");
-    }
 }
+
+
+
+
+
