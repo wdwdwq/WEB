730d0f0abd7f7840bdd44088649d20a26d3a08b2
wdwdwq <qkenr7278@gmail.com>
2024-06-17 11:29:52 +0900

    문제 - 1부터 n까지의 합을 반환하는 함수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,40 +2,38 @@ package org.example; // org.example 패키지에 속해 있음
 
 public class Main {
     public static void main(String[] args) {
-        int 결과;
+        // 예시 시작
+        int c = 20 + Math.더하기(10, 20);
+        System.out.println(c);
+        // 예시 끝
 
-        결과 = 계산기.합(10, 20);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 30
+        int 결과1 = Math.oneToSum(3);
+        System.out.println("결과1 : " + 결과1);
+        // 출력 : 결과1 : 6
 
-        결과 = 계산기.합(30, 20);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 50
-
-        결과 = 계산기.합(30, 70);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 100
-
-        결과 = 계산기.차(30, 70);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : -40
-
-        결과 = 계산기.곱(3, 7);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 21
+        int 결과2= Math.oneToSum(10);
+        System.out.println("결과2 : " + 결과2);
+        // 출력 : 결과2 : 55
     }
 }
 
-class 계산기 {
-    public static int 합(int a, int b) {
-        return a + b;
+class Math {
+    // static => 본사직원
+    // static은 현재 클래스에 지금당장 존재한다.
+    static int 더하기(int a, int b) {
+        // 메인 메서드의 int c와 아래 int c는 전혀 관계가 없다.
+        int c = a + b;
+
+        // return 값의 타입과 함수명의 앞부분이 같아야 한다.
+        // return => 변신
+        return c;
     }
 
-    public static int 차(int a, int b) {
-        return a - b;
+    public static int oneToSum(int i) {
+    int sum = 0;
+    for(int j = 1; j <= i; j++){
+        sum += j;
     }
-
-    public static int 곱(int a, int b) {
-        return a * b;
+    return  sum;
     }
 }
