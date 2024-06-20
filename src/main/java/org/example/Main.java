21cecc7223245cefca99957a8ad7e91f52fb34d3
wdwdwq <qkenr7278@gmail.com>
2024-06-20 13:41:26 +0900

    article wrtie, article list 구현 설명

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,69 +1,7 @@
-package org.example;// 문제 : 매개변수를 사용해서 전사가 매번 다르게 공격하도록 해주세요.
+package org.example;
 
-
-//올바른 리턴타입으로 메서드를 만들어주세요
-public class
-Main {
+public class Main {
     public static void main(String[] args) {
-        int i = 로봇.get정수();
-        boolean b = 로봇.get논리();
-        사람1 a사람 = 로봇.get사람();
-        사람1 a사람2 = 로봇.get사람2();
-        사람1 a사람3 = 로봇.get사람3();
-        사람1 a사람4 = 로봇.get사람4();
-        사람1 a사람5 = 로봇.get사람5(123,false);
-        로봇.get사람6(123,false);
 
-    }
-}
-
-class 사람1{
-    int age;
-    boolean gender;
-
-    사람1(){
-        this.age = 0;
-        this.gender = true;
-    }
-    사람1(int age, boolean gender){
-        this.age = age;
-        this.gender = gender;
-    }
-}
-
-
-class 로봇 {
-    public static int get정수() {
-        return 1;
-    }
-
-    public static boolean get논리() {
-        return true;
-
-    }
-
-    public static 사람1 get사람() {
-        return new 사람1();
-    }
-
-
-    public static 사람1 get사람2() {
-        return new 사람1();
-    }
-
-    public static 사람1 get사람3() {
-        return new 사람1();
-    }
-
-    public static 사람1 get사람4() {
-        return new 사람1();
-    }
-
-    public static 사람1 get사람5(int age, boolean gender) {
-        return new 사람1();
-    }
-
-    public static void get사람6(int age, boolean gender) {
-        사람1 새로운사람 = new 사람1(age, gender);
     }
 }
--- src/main/java/org/example/Main2.java
+++ src/main/java/org/example/Main2.java
@@ -1,20 +1,74 @@
 package org.example;
 
-public // 문제 : 아래가 실행되도록 해주세요.
-
-class Main2 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+// while ~하는 동안에
+// if 만약에
+// for 이거라면
+public class Main2 {
     public static void main(String[] args) {
-        사람2 a사람 = new 사람2();
+        System.out.println("==프로그램 시작==");//코드를 시작하자마자 화면에 보여줌
 
-        a사람.나이 = 20;
-        a사람.a왼팔 = new 팔();
+        Scanner sc = new Scanner(System.in); // 사용자의 입력을 Scanner로 입력 받을거야
+
+        int lasArticleId = 0; // 마지막 생성된 게시물 id를 저장할 변수
+
+        List<Article> articles = new ArrayList<>(); //articles라는 배열안에 게시물'들'을 저장할거야
+
+        while (true){//이 프로그램이 진행할 동안 계속 될거야
+            System.out.printf("명령어) ");// 실행된다면 명령어를 받을 준비를 해
+            String cmd = sc.nextLine().trim();// 입력되는 명령어 앞뒤 공백을 제거해
+
+            if(cmd.length() == 0){
+                System.out.println("명령어를 입력해주세요");
+                 continue;
+            }
+            if(cmd.equals("끝")){ // 끝이라고 입력하면 루프를 종료한다
+                break; // 종료
+            }
+            if(cmd.equals("article write")){ // article write를 입력력 게시물 작성 기능을 시작
+                lasArticleId++ ; // 게시물 id를 생성하고
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+                System.out.printf("내용 : ");
+                String body = sc.nextLine();
+                Article article = new Article(lasArticleId, title, body); //제목을 입력받아 Article 객체를 생성함
+                articles.add(article);//article 리스트에 추가하고, 게시물 생성 메시지를 출력한다.
+            }
+            else if(cmd.equals("article list")){// 만약에 article list를 입력하면 명령어를 실행하는데
+                if(articles.size() == 0){ // articles리스트 안에 아무것도 없으면
+                    System.out.println("게시물이 존재하지 않습니다");// 게시물이 존재하지 않습니다를 출력해라
+                    continue;// 그 다음 프로그램 처음으로 돌아간다
+                }
+
+                System.out.println("번호  /   제목");
+                for(int i = articles.size() -1; i >= 0; i--){
+                    //int i = articles.size() - 1 article.size는 articles의 리스트 요소 개수를 반환다
+                    //-1은 리스트의 마지막 요소 개수를 반환한다.(인덱스는 0부터 시작하기 때문에 크기에서 1을 뺀다)
+                    //인덱스가 0 이상일 때 실행된다
+                    //i-- 반복이 끝날때마다 반복해서 i의 값을 감소시킨다
+                    //리스트의 인덱스가 처음(0)까지 도달할 때까지 루프를 실행한다
+                    Article article = articles.get(i);//i 해당하는 Article 를 반환한다
+                    System.out.printf("%d	/	%s\n",article.id,article.title);
+                }// 확면에 보여준다 id(번호),title(제목)
+            }
+        }
+        System.out.println("==프로그램 끝=="); // 모든게 끝나면 프로그램이 끝난다
+
+        sc.close(); // Scanner를 닫는
     }
 }
 
-class 사람2 {
-    int 나이;
-    팔 a왼팔;
-}
+class Article {// Article이라는 클래스를 생성
+    int id;// id값을 주고
+    String title;// 문자열인 제목
+    String body;// 문자열인 내용
 
-class 팔 {
+    Article(int id, String title, String body){// 생성자를 통해 객체를 초기화함
+        this.id = id; //번호
+        this.title = title; // 제목
+        this.body = body; // 내용
+
+    }
 }
