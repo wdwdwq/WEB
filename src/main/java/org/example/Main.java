af07be0fbb07c026c7d2c068123abf02d3966a40
wdwdwq <qkenr7278@gmail.com>
2024-07-01 13:26:43 +0900

    처음부터 다시 복습

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        new App(sc).run();
-
-
-        sc.close();
-
     }
 }
--- src/main/java/org/example/System/SystemController.java
+++
@@ -1,4 +0,0 @@
-package org.example.System;
-
-public class SystemController {
-}
